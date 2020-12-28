38b8f8fff81a8932584b9cb7c1904fa9348f13ec
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-12-28 22:55:51 +0000

    Add server entry-point & start of client handling

---
+++ server/src/main/kotlin/server/ClientHandler.kt
@@ -0,0 +1,10 @@
+package server
+
+import java.net.Socket
+
+class ClientHandler(server: Server, clientSocket: Socket, board: Board) {
+
+    fun start() {
+        //TODO
+    }
+}
---
+++ server/src/main/kotlin/server/Server.kt
@@ -0,0 +1,28 @@
+package server
+
+import java.io.IOException
+import java.net.ServerSocket
+
+class Server(private val port: Int) : Thread() {
+    var handlers: MutableList<ClientHandler> = mutableListOf()
+
+    var gameBoard = Board()
+
+    override fun run() {
+        try {
+            val serverSocket = ServerSocket(port)
+            while (true) {
+                println("Accepting client connections..")
+
+                val clientSocket = serverSocket.accept()
+                println("Accepting new connection from $clientSocket")
+
+                val clientHandler = ClientHandler(this, clientSocket, gameBoard)
+                handlers.add(clientHandler)
+                clientHandler.start()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
