0f53949015976a9213edf70cbeaa2cc89ce3f35f
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-12-28 23:15:37 +0000

    Add getter for client list & refactor

--- server/src/main/kotlin/server/Server.kt
+++ server/src/main/kotlin/server/Server.kt
@@ -4,9 +4,14 @@ import java.io.IOException
 import java.net.ServerSocket
 
 class Server(private val port: Int) : Thread() {
-    var handlers: MutableList<ClientHandler> = mutableListOf()
+    private val handlers: MutableList<ClientHandler> = mutableListOf()
 
-    var gameBoard = Board()
+    private val gameBoard = Board()
+
+    val getHandlers
+        get(): MutableList<ClientHandler> {
+            return handlers
+        }
 
     override fun run() {
         try {
